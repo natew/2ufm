75751bf87a344cd84b290aa8b9b36c6c85c1152c
Nate Wienert <natew@me.com>
2011-08-18 00:22:35 -0700

    popular

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -15,7 +15,10 @@ class MainController < ApplicationController
   end
   
   def popular
+    cols   = Song.column_names.collect {|c| "songs.#{c}"}.join(",")
+    where  = " WHERE games.created_at > '#{params[:within].to_i.days.ago.to_s(:db)}'" unless params[:within].nil?
     
+    @songs = Song.find_by_sql "SELECT songs.*, count(favorites.id) as favorites_count FROM songs INNER JOIN favorites on favorites.favorable_id = songs.id and favorites.favorable_type = 'Song'#{where} GROUP BY favorites.favorable_id, #{cols} ORDER BY favorites_count DESC LIMIT 21"
     
     respond_to do |format|
       format.html
