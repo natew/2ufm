56396e2020076510271f8af90863ceed43343609
natew <natewienert@gmail.com>
2012-05-08 23:58:50 -0700

    quickpush

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -143,22 +143,29 @@ $(function() {
   //
   // Application integration
   //
-  document.addEventListener('play', function() {
-    mp.toggle();
-    showGrowlInfo();
-  }, true);
-  document.addEventListener('prev', function() {
-    mp.prev();
-    showGrowlInfo();
-  }, true);
-  document.addEventListener('next', function() {
-    mp.next();
-    showGrowlInfo();
-  }, true);
+  if (typeof macgap !== 'undefined') {
+    document.addEventListener('play', function() {
+      mp.toggle();
+      showGrowlInfo();
+    }, true);
+    document.addEventListener('prev', function() {
+      mp.prev();
+      showGrowlInfo();
+    }, true);
+    document.addEventListener('next', function() {
+      mp.next();
+      showGrowlInfo();
+    }, true);
 
-  function showGrowlInfo() {
-    var info = mp.curSongInfo();
-    macgap.growl.notify({title: info.artist_name + " - " + info.name, content: 'Now playing'});
+    function showGrowlInfo() {
+      var info = mp.curSongInfo();
+      macgap.growl.notify({title: info.artist_name + " - " + info.name, content: 'Now playing'});
+    }
+
+    $('header h1 a').click(function() {
+      window.location = window.location;
+      return false;
+    });
   }
 
   // Play from playlist
