d1a42010d8f787a207c6024fab52fd88de3289c0
natew <natewienert@gmail.com>
2012-07-07 00:01:05 -0700

    fix some styling, broadcasts based on shared_id

--- app/controllers/broadcasts_controller.rb
+++ app/controllers/broadcasts_controller.rb
@@ -3,7 +3,7 @@ class BroadcastsController < ApplicationController
 
   def create
     @song = Song.find(params[:song_id])
-    @broadcast = current_user.station.broadcasts.create(:song_id => @song.id)
+    @broadcast = current_user.station.broadcasts.create(:song_id => @song.shared_id)
     @locals = { :action => 'remove', :id => @broadcast.id, :count => @song.user_broadcasts_count+1 }
 
     respond_to do |format|
