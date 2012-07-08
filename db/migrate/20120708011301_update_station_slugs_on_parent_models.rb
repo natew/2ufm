127c94c8edbe872ec240c3011d49642dd5e3f9be
natew <natewienert@gmail.com>
2012-07-07 21:09:31 -0700

    quickpush

--- db/migrate/20120708011301_update_station_slugs_on_parent_models.rb
+++ db/migrate/20120708011301_update_station_slugs_on_parent_models.rb
@@ -1,16 +1,19 @@
 class UpdateStationSlugsOnParentModels < ActiveRecord::Migration
   def up
     User.all.each do |model|
+      model.make_station if model.station.nil?
       model.set_station_slug
       model.save
     end
 
     Artist.all.each do |model|
+      model.make_station if model.station.nil?
       model.set_station_slug
       model.save
     end
 
     Blog.all.each do |model|
+      model.make_station if model.station.nil?
       model.set_station_slug
       model.save
     end
