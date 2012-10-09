aef5dc6197cc03ed15c896a80bad3b473027c67b
Nate Wienert <nwienert@zappos.com>
2012-10-09 16:07:10 -0700

    quickdeploy $*

--- vendor/assets/javascripts/jquery.allOn.js
+++ vendor/assets/javascripts/jquery.allOn.js
@@ -1,10 +1,8 @@
 $.fn.allOn = function(onEvent, bindings) {
-  fn.log(bindings);
   for (var target in bindings) {
     (function(t) {
       $(this).on(onEvent, t, function(e) {
-        fn.log('calling', t);
-        bindings[t].call(e, $(this));
+        $.proxy(bindings[t].call(e, $(this)), this);
       });
     })(target);
   }
