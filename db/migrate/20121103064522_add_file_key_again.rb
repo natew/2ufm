81e5e60d302edb1dfd2ef996023da0ff91fdf1ac
natew <natewienert@gmail.com>
2012-11-04 01:27:47 -0700

    quickpush fix migrations

--- db/migrate/20121103064522_add_file_key_again.rb
+++ db/migrate/20121103064522_add_file_key_again.rb
@@ -1,10 +1,6 @@
 class AddFileKeyAgain < ActiveRecord::Migration
   def up
     add_column :songs, :file_key, :string
-
-    Song.where('songs.file_key is null').each do |song|
-      song.delay.set_file_key_and_save
-    end
   end
 
   def down
--- db/migrate/20121104073726_change_songs_hash_to_token.rb
+++ db/migrate/20121104073726_change_songs_hash_to_token.rb
@@ -1,5 +1,13 @@
 class ChangeSongsHashToToken < ActiveRecord::Migration
-  def change
+  def up
     rename_column :songs, :hash, :token
+
+    Song.where('songs.token is null').each do |song|
+      song.delay.set_token_and_save
+    end
+  end
+
+  def down
+    rename_column :songs, :token, :hash
   end
 end
