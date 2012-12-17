634edb6be769735b5944e6d2c9099b3e018caa72
natew <natewienert@gmail.com>
2012-12-16 22:08:30 -0800

    quickpush

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,20 +4,16 @@ class UserMailer < ActionMailer::Base
   def follow(user, followee)
     @follower = user
     @followee = followee
-    @unsubscribe_type = 'follow'
-    set_unsubscribe_key(@followee)
-    logger.info "erakndasndjknadklsandksandlsandkaslndsakldnaslkdnaskldnslakdnsalknd"
-    logger.info from + "------------------------------"
-    mail(from: from, to: @followee.email, subject: "#{@follower.full_name} is now following you on 2u.fm") do |format|
+    set_unsubscribe('follow', @followee)
+    mail(to: @followee.email, subject: "#{@follower.full_name} is now following you on 2u.fm") do |format|
       format.html { render 'follow' }
     end
   end
 
   def share(sender, share)
     @sender, @receiver, @song = sender, share.receiver, share.song
-    set_unsubscribe_key(@receiver)
-    @unsubscribe_type = 'share'
-    mail(from: from, to: @receiver.email, subject: "#{sender.full_name} sent you a song on 2u.fm") do |format|
+    set_unsubscribe('share', @receiver)
+    mail(to: @receiver.email, subject: "#{sender.full_name} sent you a song on 2u.fm") do |format|
       format.html { render 'share' }
     end
   end
@@ -29,7 +25,7 @@ class UserMailer < ActionMailer::Base
 
     if @shares.count or @follows.count
       date = Time.now.strftime("%b #{return_date.day.ordinalize}")
-      mail(from: from, to: user.email, subject: "New Happenings on 2u.fm Today") do |format|
+      mail(to: user.email, subject: "New Happenings on 2u.fm Today") do |format|
         format.html { render 'daily_digest' }
       end
     end
@@ -37,11 +33,8 @@ class UserMailer < ActionMailer::Base
 
   private
 
-  def from
-    "\"2u.fm\" <noreply@2u.fm>"
-  end
-
-  def set_unsubscribe_key(user)
+  def set_unsubscribe(type, user)
+    @unsubscribe_type = type
     @unsubscribe_key = user.confirmation_token
   end
 end
