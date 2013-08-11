68a03e141d62d49151f20916fefdc7662920a3f2
natew <natewienert@gmail.com>
2013-08-11 00:36:41 -0700

    quickpush more fixes on migrations

--- db/migrate/20120720042259_redo_index_from_shared_id.rb
+++ db/migrate/20120720042259_redo_index_from_shared_id.rb
@@ -1,6 +1,5 @@
 class RedoIndexFromSharedId < ActiveRecord::Migration
   def change
-    remove_index :songs, [:processed, :working, :rank, :shared_id]
     add_index :songs, [:processed, :working, :rank, :matching_id]
   end
 end
