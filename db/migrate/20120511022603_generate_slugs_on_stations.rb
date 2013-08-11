34fca87f06afeb73cf6dde131d1e6cc32cc3b253
natew <natewienert@gmail.com>
2013-08-11 00:54:34 -0700

    quickpush no more stuff in migrations

--- db/migrate/20120511022603_generate_slugs_on_stations.rb
+++ db/migrate/20120511022603_generate_slugs_on_stations.rb
@@ -1,9 +1,9 @@
 class GenerateSlugsOnStations < ActiveRecord::Migration
   def up
-    Station.all.each do |station|
-      station.slug = station.title.to_url
-      station.save
-    end
+    # Station.all.each do |station|
+    #   station.slug = station.title.to_url
+    #   station.save
+    # end
   end
 
   def down
